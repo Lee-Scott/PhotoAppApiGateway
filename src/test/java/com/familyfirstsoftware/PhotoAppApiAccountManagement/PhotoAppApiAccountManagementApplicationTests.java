902edecdf857780d86d9da954f9083329dc3aba7
Lee-Scott <leehscott24@gmail.com>
2022-04-21 17:53:17 -0700

    Basic login functionality done.

---
+++ src/test/java/com/familyfirstsoftware/PhotoAppApiAccountManagement/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familyfirstsoftware.PhotoAppApiAccountManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
